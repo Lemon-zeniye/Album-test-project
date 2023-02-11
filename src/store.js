0349032c81b4dfc1042872784588a25ef0a6e8a4
Lemon Abate <endalklemon.abate@gmail.com>
2023-02-11 11:53:16 +0300

    provide saga as middleware to the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { albumSlice } from "./Features/albumSlice"
+import { albumSlice } from "./Features/albumSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import albumSaga from "./Saga/albumSaga";
+
+const saga = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         albums: albumSlice
-    }
+    },
+    middleware: [saga]
 });
+
+saga.run(albumSaga);
 
 export default store;
