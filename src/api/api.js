c286318c5865c2a65ac84832aea837c222a4dec6
Lemon Abate <endalklemon.abate@gmail.com>
2023-02-11 14:25:08 +0300

    bug fix

--- src/api/api.js
+++ src/api/api.js
@@ -4,9 +4,9 @@ axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
 
 export const getAlbums = async () => await axios.get("/albums");
 
-export const createAlbum = async () => await axios.post("/album", album);
+export const createAlbum = async (album) => await axios.post("/album", album);
 
-export const updateAlbum = async () => await axios.put(`/album/${album.id}`, album);
+export const updateAlbum = async (album) => await axios.put(`/album/${album.id}`, album);
 
-export const deleteAlbum = async () => await axios.delete(`/album/${id}`);
+export const deleteAlbum = async (id) => await axios.delete(`/album/${id}`);
 
