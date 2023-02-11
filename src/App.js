bd6a6e3c1b502751ce0b177a7084cc329faf5f68
Lemon Abate <endalklemon.abate@gmail.com>
2023-02-11 13:50:40 +0300

    dispach an action and get the state data using useSelector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getAllAlbumsStart } from "./Features/albumSlice";
+
+
+
 function App() {
+  const albums = useSelector(state => state.albums.albums);
+  const dispatch = useDispatch();
+  console.log(albums)
+
+  useEffect(() => {
+    dispatch(getAllAlbumsStart());
+  },[])
   return (
     <div>
       
